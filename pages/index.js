0e4b7e0f2cbc15d3eb6602b8def0ad19d09753df
Samuel Patrick <patrick.samuellopes@gmail.com>
2025-09-18 02:13:27 +0000

    Colocando uma frase amorosa

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home (){
-    return <h1>Correção</h1>
+    return <h1>Aitana, eu amo vc. Quero passar o resto dos meus dias juntos a ti, sorrindo, jogando e te estressando!</h1>
 }
 
 export default Home;
